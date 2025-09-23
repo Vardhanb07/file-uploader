7c78707a43d824914195c8931b7c10fbd344df7b
Vardhan Battula <170970431+Vardhanb07@users.noreply.github.com>
2025-09-23 18:59:59 +0530

    Add index, login, signup pages

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 require("dotenv").config();
+const loginRouter = require("./routes/loginRouter");
+const signupRouter = require("./routes/signupRouter");
 
 const app = express();
 
@@ -9,10 +11,16 @@ app.set("view engine", "ejs");
 app.set("views", "./views");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/login", loginRouter);
+app.use("/signup", signupRouter);
 
 app.get("/", (req, res) => {
-  res.render("index")
+  res.render("index");
 });
+
+app.use((req, res, next) => {
+  res.status(404).render("404")
+})
 
 app.listen(port, () => {
   console.log(`localhost: http://localhost:${port}`);
